162d464cbeae17b8aedf74a47a750dcbfd9cbebd
LUO-Ming <m.luo@innox-jp.com>
2017-04-11 17:23:18 +0900

    update master
    update master
    update master

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,8 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World from branch01 !");
-        System.out.println("Hello World from branch02 !");
+    	System.out.println("Hello World from branch01 !");
+    	System.out.println("Hello World from branch02 !");
         System.out.println("Hello World from branch03 !");
     }
 }
